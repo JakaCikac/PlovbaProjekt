e3e7f341c90f2d15a06faca1b815b3d2df4c19f4
Juš Lozej <jlozej@gmail.com>
2016-05-02 16:46:37 +0200

    Basic movement script

---
+++ unity/Plovba/Assets/rowScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rowScript : MonoBehaviour {
+    Rigidbody rb;
+    public float RowForce = 10;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (Input.GetKey("a"))
+        {
+
+            rb.AddForceAtPosition(transform.forward * RowForce, transform.TransformPoint( new Vector3(-0.2f , 0, 0 )));
+        }
+        else if (Input.GetKey("d"))
+        {
+            rb.AddForceAtPosition(transform.forward * RowForce, transform.TransformPoint( new Vector3(0.2f , 0, 0 )));
+        }
+    }
+}
